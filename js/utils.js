10df8cfea0f58a14a315f9f51004268aa39fc7b7
ndserg <ndserg@gmail.com>
2020-07-08 00:31:07 +0300

    14. Личный проект: модуляция (часть 1)

---
+++ js/utils.js
@@ -0,0 +1,83 @@
+'use strict';
+(function () {
+  window.util = {
+    // Добавление класса элементам
+    addClassOfElement: function (item, itemClass) {
+      if (item.classList.contains(itemClass) === false) {
+        item.classList.add(itemClass);
+      }
+    },
+
+    // Удаление класса элементам
+    removeClassOfElement: function (item, itemClass) {
+      if (item.classList.contains(itemClass) === true) {
+        item.classList.remove(itemClass);
+      }
+    },
+
+    // Установка атрибута элемента
+    setElementAttribute: function (item, attrName, attrValue) {
+      if (item.hasAttribute(attrName, attrValue) === false) {
+        item.setAttribute(attrName, attrValue);
+      }
+    },
+
+    // Удаление атрибута элемента
+    removeElementAttribute: function (item, attrName, attrValue) {
+      if (item.hasAttribute(attrName, attrValue) === true) {
+        item.removeAttribute(attrName, attrValue);
+      }
+    },
+
+    // Установка/удаление атрибута всем элементам управления формы
+    formControlsToggle: function (toggleState, formControls, attrName, attrValue) {
+      var toggleSetting = window.util.setElementAttribute;
+
+      if (toggleState === 'remove') {
+        toggleSetting = window.util.removeElementAttribute;
+      }
+
+      for (var i = 0; i < formControls.length; i++) {
+        toggleSetting(formControls[i], attrName, attrValue);
+      }
+    },
+
+    //  Генератор окончаний числительных
+    numbersEnding: function declination(number, titles) {
+      var cases = [2, 0, 1, 1, 1, 2];
+      return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    },
+
+    //  Генератор случайных чисел
+    generateRandomNumber: function getRandomInt(min, max) {
+      return (Math.floor(Math.random() * (max - min + 1)) + min);
+    },
+
+    //  Случайная выборка из массива
+    getRandomList: function (sourceList) {
+      var randomList = sourceList.slice();
+
+      randomList.forEach(function (item, index) {
+        var j = window.util.generateRandomNumber(0, randomList.length - 1);
+        var temp = randomList[index];
+        randomList[index] = randomList[j];
+        randomList[j] = temp;
+        return randomList;
+      });
+
+      randomList.length = window.util.generateRandomNumber(1, sourceList.length);
+      return randomList;
+    },
+
+    //  Координаты блока относительно блока MAP
+    getCoordinates: function getCoord(item, itemWidth, itemHeight) {
+      var pinItem = item.getBoundingClientRect();
+      var pinCoordGapX = Math.floor((document.querySelector('html').offsetWidth - window.map.offsetWidth) / 2);
+
+      return {
+        itemY: Math.floor(pinItem.top + pageYOffset + itemHeight),
+        itemX: Math.floor(pinItem.left + pageXOffset - pinCoordGapX - itemWidth / 2)
+      };
+    }
+  };
+})();
